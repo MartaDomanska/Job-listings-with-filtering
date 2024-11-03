3d62126e3cc85dcf1d7a4585fadf0c2389be1f50
MartaDomanska <martadomanska@MacBook-Pro-MartaDomanska.local>
2024-11-03 18:18:56 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>Job Listings</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
